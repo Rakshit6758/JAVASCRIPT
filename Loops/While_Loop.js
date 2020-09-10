98223503c27a74a105b9d59112b7366150a98c28
Rakshit6758 <500067642@stu.upes.ac.in>
2020-09-10 21:30:48 +0530

    While Loop javascript file created

---
+++ Loops/While_Loop.js
@@ -0,0 +1,10 @@
+var count=0;
+document.write("Loop Starts.....<br><br>");
+
+while(count < 10)
+{
+    document.write("current count="+ count + "<br>");
+    count++;
+}
+
+document.write("<br>Loop Stops!!!");

c09273a0e28897f9cc263db60f47ec126f0b88ec
Rakshit6758 <500067642@stu.upes.ac.in>
2020-09-10 18:09:57 +0530

    Switch case javascript file

---
+++ SwitchCase/Switch_Case.js
@@ -0,0 +1,24 @@
+var grade=window.prompt("Enter your Grade");
+
+switch(grade){
+    case 'A':document.write("EXCELLENT");
+    break;
+
+    case 'B':document.write("GOOD JOB");
+    break;
+
+    case 'C':document.write("AVERAGE");
+    break;
+
+    case 'D':document.write("WORK HARD");
+    break;
+    
+    case 'E':document.write("YOUR TIME HAS COME");
+    break;
+
+    case 'F':document.write("FAIL");
+    break;
+
+    default:document.write("Unknown Grade");
+    }
+

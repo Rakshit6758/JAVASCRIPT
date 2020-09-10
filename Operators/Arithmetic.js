4000c70b4325b0b07f417e26dc8b4d0c80d30f97
Rakshit6758 <500067642@stu.upes.ac.in>
2020-09-10 17:37:02 +0530

    Javascript files further created

---
+++ Operators/Arithmetic.js
@@ -0,0 +1,39 @@
+var a=25;
+var b=20;
+var c="hi";
+var linebreak="<br />";
+
+document.write("a + b = ");
+result=a+b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a - b = ");
+result=a-b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a * b = ");
+result=a*b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a / b = ");
+result=a/b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a % b = ");
+result=a%b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a + b + c = ");
+result=a+b+c;
+document.write(result);
+document.write(linebreak);
+
+document.write("a++");
+result=a++;
+document.write(result);
+document.write(linebreak);
---
+++ Operators/Comparison.js
@@ -0,0 +1,33 @@
+var a=20;
+var b=30;
+var linebreak="<br />";
+
+document.write("(a > b) =>");
+result=a>b;
+document.write(result);
+document.write(linebreak);
+
+document.write("(a < b) =>");
+result=a<b;
+document.write(result);
+document.write(linebreak);
+
+document.write("(a == b) =>");
+result=a==b;
+document.write(result);
+document.write(linebreak);
+
+document.write("(a <= b) =>");
+result=a<=b;
+document.write(result);
+document.write(linebreak);
+
+document.write("(a >= b) =>");
+result=a>=b;
+document.write(result);
+document.write(linebreak);
+
+document.write("(a != b) =>");
+result=a!=b;
+document.write(result);
+document.write(linebreak);

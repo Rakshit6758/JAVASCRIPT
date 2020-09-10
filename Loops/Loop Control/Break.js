309dc36c3173da689f2984e0820daa66e3338c32
Rakshit6758 <500067642@stu.upes.ac.in>
2020-09-10 22:00:23 +0530

    Break Statement

---
+++ Loops/Loop Control/Break.js
@@ -0,0 +1,11 @@
+var x = 1;
+ document.write("Loop Starts..<br><br>");
+
+ while(x < 10)
+ if(x == 5)
+{
+    break;
+}
+  x = x + 1;
+  document.write(x + "<br>");
+document.write("<br><br>Loop Ends!!!!!");

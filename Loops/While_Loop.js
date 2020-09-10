ae0c7f3fd525fea24bddebc12b64eb7e2483c162
Rakshit6758 <500067642@stu.upes.ac.in>
2020-09-10 21:48:34 +0530

    Chnages made

--- Loops/While_Loop.js
+++ Loops/While_Loop.js
@@ -3,7 +3,7 @@ document.write("Loop Starts.....<br><br>");
 
 while(count < 10)
 {
-    document.write("current count="+ count + "<br>");
+    document.write("current While Count="+ count + "<br>");
     count++;
 }
 

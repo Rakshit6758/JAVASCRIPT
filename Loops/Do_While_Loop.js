0a1b84494bbe4efd4ef1ce2f28245c987cce913e
Rakshit6758 <500067642@stu.upes.ac.in>
2020-09-10 21:42:10 +0530

    do while loop javascript file ceated

---
+++ Loops/Do_While_Loop.js
@@ -0,0 +1,10 @@
+var a = 0;
+ document.write("Loop Starts.....<br><br>");
+
+ do{
+     document.write("Current Count:" + a +"<br>");
+     a++
+ }
+ while(a < 10);
+
+ document.write("<br>Loop Ends!!!");

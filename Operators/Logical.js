6479af4f302787b4e9a179af3ca4031b371e6b06
Rakshit6758 <500067642@stu.upes.ac.in>
2020-09-11 20:16:06 +0530

    logical Operators javascript file created

---
+++ Operators/Logical.js
@@ -0,0 +1,18 @@
+var a = true;
+var b = false;
+var linebreak = "<br />";
+
+document.write("(a && b) => ");
+result = (a && b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a || b) => ");
+result = (a || b);
+document.write(result);
+document.write(linebreak);
+
+document.write("!(a && b) => ");
+result = (!(a && b));
+document.write(result);
+document.write(linebreak);

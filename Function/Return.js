8543d9eba16d302e24b31b0f6529eba4ff7de136
Rakshit6758 <500067642@stu.upes.ac.in>
2020-09-22 19:22:15 +0530

    Return javascript file Created

---
+++ Function/Return.js
@@ -0,0 +1,11 @@
+function kya(first, last){
+    var full;
+    full = first + last;
+    return full;
+}
+
+function baat(){
+    var result;
+    result= kya("Saif", "Ali");
+    document.write(result);
+}

c7a5062f34458fcdb497750fb204696eebdfbcb1
Rakshit6758 <500067642@stu.upes.ac.in>
2020-10-14 21:39:29 +0530

    javascript file added

---
+++ Objects/Object() Constructor/Example1/Script 1.js
@@ -0,0 +1,6 @@
+var book = new Object();
+book.subject = "perl";
+book.author = "Rakshit";
+
+document.write("Book name is: " + book.subject + "<br>");
+document.write("Book author is: " + book.author + "<br>");
